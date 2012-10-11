e8dc04131ccf37be1d086502419e29a4036c63a7
adahn <adahn@desktop-linux>
2012-10-11 18:32:14 +0200

    Dodany projekt eclipsowy do testowania monitora

---
+++ monitor/src/sensorSide/SelectorTest.java
@@ -0,0 +1,16 @@
+package sensorSide;
+import java.io.IOException;
+import java.nio.channels.*;
+
+public class SelectorTest {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+
+		
+	}
+
+}

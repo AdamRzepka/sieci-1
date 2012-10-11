2b94c6c1fa60fb7397c665c4f4e9fa858c56553a
adahn <adahn@desktop-linux>
2012-10-11 15:18:36 +0200

    client side TCP test code

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,3 +1,18 @@
 import metrics
+import socket
+
+BUFFER_SIZE = 2048
+address = 127.0.0.1
+port = 12087
+message = 'message from sensor'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((address, port))
+print 'Connected to server'
+s.send(message)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print 'Message from server: %s', data
 
 print metrics.cpu_stat.procs_running()

48a37c6e4c72ec4e806037d794de7851563accf6
adahn <adahn@desktop-linux>
2012-10-13 13:19:13 +0200

    Odbieranie wiadomości usunięte z kodu sensora

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -10,9 +10,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((address, port))
 print 'Connected to server'
 s.send(message)
-data = s.recv(BUFFER_SIZE)
 s.close()
 
-print 'Message from server: ', data
+print 'Message sent'
 
 print metrics.cpu_stat.procs_running()
